9512fe2f0bb0f1a0186f6f6c8e1ebacfec6f2fa8
Mankarka <111666967+Mankarka@users.noreply.github.com>
2022-10-02 20:37:47 -0400

    Create myGetIP.py

---
+++ python/myGetIP.py
@@ -0,0 +1,9 @@
+def getHostnameByIP(h):
+
+try:
+  hostname = str(h)
+  ip = socket.gethostbyname(hostname)
+  print (hostname +' has an IP of ' + ip)
+
+except:
+  print("Oops, something is wrong with that host")
