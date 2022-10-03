54c8317336fdc28f5f8f3e9f314678fca77a79fa
Mankarka <111666967+Mankarka@users.noreply.github.com>
2022-10-02 20:22:53 -0400

    Update nameAge.py

--- python/nameAge.py
+++ python/nameAge.py
@@ -1,1 +1,15 @@
+from msvcrt import kbhit
+import time
+start_time = time.time()
 
+
+print('What is your name?')
+myName = input()
+print ('Hello, ' + myName + ' . That is a good name. How old are you?')
+myAge = input()
+programAge = int(time.time() - start_time)
+print('%s? that"s funny, i"m only %s seconds old.' % (myAge, programAge ))
+print('i wish i was %s years old' %(myAge *2))
+
+time.sleep(3)
+print('I"m tired. I go sleep sleep now'
