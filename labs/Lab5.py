0f5de1ffaec344ee46c8156304ea94332391923b
Mankarka <111666967+Mankarka@users.noreply.github.com>
2022-10-04 00:33:55 -0400

    Lab5.py

--- labs/Lab5.py
+++ labs/Lab5.py
@@ -1,14 +1,13 @@
 import datetime
 
-
 while True:
     try:
         bday = input("Please enter your exact Date of Birth(eg:-10 02 2022):")
         birthday = datetime.datetime.strptime(bday, '%m %d %Y')
         break
     except:
-        print("Please follow the format Month: 01-12 Day:01-31 year:1980-2022, dont in")
+        print("Please follow the format Month: 01-12 Day:01-31 year:1980-2022, dont")
 
 tday = datetime.datetime.today()
-timedelta = (tday - birthday).total_seconds()
-print("You have been alive for:",timedelta,"seconds")
+timeSec = (tday - birthday).total_seconds()
+print("You have been alive for:",timeSec,"seconds")
